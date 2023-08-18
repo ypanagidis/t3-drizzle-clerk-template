f71e02016bbe7d0e74f65cbd79844c70f0f04bdb
yiannis-pan <63311172+yiannis-pan@users.noreply.github.com>
2023-08-18 17:27:01 +0300

    added dark mode, dark mode switcher, next-themes and dropdDown comp (#1)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "~/components/ui/theme-provider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </ClerkProvider>
   );
 };
